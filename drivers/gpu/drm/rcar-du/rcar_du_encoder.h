d1f9f30dbc2dc3112cf4c4ddff1b83af04558ca7
Biju Das <biju.das.jz@bp.renesas.com>
2022-07-25 15:42:52 +0100

    drm: rcar-du: Add encoder lib support
    Add RCar DU encoder lib support by moving rcar_du_encoder_count_ports()
    and rcar_du_encoder_funcs to the lib file and added
    rcar_du_encoder_funcs() to share the common code between RCar and
    RZ/G2L DU encoder drivers.
    
    Signed-off-by: Biju Das <biju.das.jz@bp.renesas.com>

--- drivers/gpu/drm/rcar-du/rcar_du_encoder.h
+++ drivers/gpu/drm/rcar-du/rcar_du_encoder.h
@@ -2,7 +2,7 @@
 /*
  * R-Car Display Unit Encoder
  *
- * Copyright (C) 2013-2014 Renesas Electronics Corporation
+ * Copyright (C) 2013-2022 Renesas Electronics Corporation
  *
  * Contact: Laurent Pinchart (laurent.pinchart@ideasonboard.com)
  */
@@ -10,17 +10,7 @@
 #ifndef __RCAR_DU_ENCODER_H__
 #define __RCAR_DU_ENCODER_H__
 
-#include <drm/drm_encoder.h>
-
-struct rcar_du_device;
-
-struct rcar_du_encoder {
-	struct drm_encoder base;
-	enum rcar_du_output output;
-};
-
-#define to_rcar_encoder(e) \
-	container_of(e, struct rcar_du_encoder, base)
+#include "rcar_du_encoder_lib.h"
 
 int rcar_du_encoder_init(struct rcar_du_device *rcdu,
 			 enum rcar_du_output output,

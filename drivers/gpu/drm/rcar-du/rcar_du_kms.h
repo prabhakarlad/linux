fe805984a06e9edd710b78af59616ca53393a558
Biju Das <biju.das.jz@bp.renesas.com>
2022-08-24 14:34:10 +0100

    drm: rcar-du: Move rcar_du_dumb_create()
    Move rcar_du_dumb_create() to RCar DU KMS lib.
    
    Signed-off-by: Biju Das <biju.das.jz@bp.renesas.com>

--- drivers/gpu/drm/rcar-du/rcar_du_kms.h
+++ drivers/gpu/drm/rcar-du/rcar_du_kms.h
@@ -13,17 +13,12 @@
 #include "rcar_du_kms_lib.h"
 
 struct dma_buf_attachment;
-struct drm_file;
 struct drm_device;
 struct drm_gem_object;
-struct drm_mode_create_dumb;
 struct rcar_du_device;
 struct sg_table;
 
 int rcar_du_modeset_init(struct rcar_du_device *rcdu);
-
-int rcar_du_dumb_create(struct drm_file *file, struct drm_device *dev,
-			struct drm_mode_create_dumb *args);
 
 struct drm_gem_object *rcar_du_gem_prime_import_sg_table(struct drm_device *dev,
 				struct dma_buf_attachment *attach,
